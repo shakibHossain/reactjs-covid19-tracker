a34cb26a85142918eca5117fb4c8be7c6e39925b
shakibHossain <shakib.hossain.sh@gmail.com>
2021-07-13 21:15:23 -0600

    add material ui grid layout on dashboard, add custom select component ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import './App.css';
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+import "./App.css";
+import { Select } from "@material-ui/core";
+import CustomSelect from "./components/custom-select/custom-select.component";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Paper className="app__paper">
+            <h4>Covid 19 Tracker</h4>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className="app__paper">
+            <CustomSelect />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className="app__paper">Cases</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className="app__paper">Recoveries</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className="app__paper">Deaths</Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className="app__paper">Table</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className="app__paper">Map</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className="app__paper">Graph</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className="app__paper">News</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
